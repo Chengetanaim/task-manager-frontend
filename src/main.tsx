ea39517b537e17fa040db8538bcf0fdfe74b4b21
chengetanai <mukanhairic@gmail.com>
2026-01-24 09:28:53 +0200

    feat: Auth Page

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,13 @@ import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { NavBar } from "./components/navbar.tsx";
 import { Index } from "./routes/index.tsx";
+import { Auth } from "./routes/auth.tsx";
 
 const router = createBrowserRouter([
+  {
+    path: "/login-or-signup",
+    element: <Auth />,
+  },
   {
     path: "/",
     element: <NavBar />,
