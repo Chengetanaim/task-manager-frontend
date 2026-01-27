dffd67bd0e9b7d77daf4d53e5232846619856539
chengetanai <mukanhairic@gmail.com>
2026-01-27 20:08:00 +0200

    feat: using camelcasekeys

--- src/utils/auth/auth-service.ts
+++ src/utils/auth/auth-service.ts
@@ -1,11 +1,18 @@
 import type { LoginCredentials, AuthResponse } from "../types";
 import { api } from "../../api/http";
+import camelcaseKeys from "camelcase-keys";
 
 
 export const authService = {
     login: async (credentials:LoginCredentials): Promise<AuthResponse> => {
-        const response = await api.post('/login', credentials)
-        return response.data
+        const formData = new URLSearchParams()
+        formData.append("username", credentials.email)
+        formData.append("password", credentials.password)
+        const response = await api.post('/login', formData, {headers: {"Content-Type": "application/x-www-form-urlencoded"}})
+        
+        const transformedData = camelcaseKeys(response.data)
+        console.log(transformedData)
+        return transformedData
     },
     logout: async(): Promise<void> => {
         await api.get('/logout')
