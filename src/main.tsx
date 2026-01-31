7b1fd0ed3305cb22603cae7aeea8467665dec35e
chengetanai <mukanhairic@gmail.com>
2026-01-31 08:11:31 +0200

    feat: global toast

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import { Index } from "./routes/index.tsx";
 import { Auth } from "./routes/auth.tsx";
 import { ProtectedRoute } from "./components/auth/protected-route.tsx";
 import { AuthProvider } from "./routes/auth-provider.tsx";
+import { Toaster } from "react-hot-toast";
 
 const router = createBrowserRouter([
   {
@@ -33,6 +34,30 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AuthProvider>
       <RouterProvider router={router} />
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          duration: 4000,
+          style: {
+            background: "#363636",
+            color: "#fff",
+          },
+          success: {
+            duration: 3000,
+            iconTheme: {
+              primary: "#4ade80",
+              secondary: "#fff",
+            },
+          },
+          error: {
+            duration: 4000,
+            iconTheme: {
+              primary: "#ef4444",
+              secondary: "#fff",
+            },
+          },
+        }}
+      />
     </AuthProvider>
   </StrictMode>,
 );
