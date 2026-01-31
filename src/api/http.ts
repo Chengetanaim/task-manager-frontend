a09c5c0b11c7db12fb2dea80fa7ec4edcf4fe7a1
chengetanai <mukanhairic@gmail.com>
2026-01-31 08:11:55 +0200

    feat: handling different types of errors

--- src/api/http.ts
+++ src/api/http.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import toast from "react-hot-toast";
 
 export const api = axios.create({
     baseURL: "http://127.0.0.1:8000",
@@ -18,10 +19,39 @@ api.interceptors.request.use(config => {
 api.interceptors.response.use(
     response => response,
     error => {
-        if (error.response?.status === 401){
-            localStorage.removeItem('taskManagertoken')
-            window.location.href = '/login-or-signup'
+        if (error.response){
+            const status = error.response.status
+            const message = error.response.data?.detail
+
+            const isLoginEndpoint = error.config?.url?.includes('login')
+             
+            switch (status) {
+                case 401:
+                    if (!isLoginEndpoint) {
+                        localStorage.removeItem('taskManagertoken')
+                        toast.error('Session expired. Please login again.')
+                        window.location.href = '/login-or-signup'
+                    }
+                    break
+                case 403:
+                    toast.error("You do not have permissions to perform this action")
+                    break
+                case 404:
+                    toast.error(message || "Resource not found.")
+                    break
+                case 500:
+                    toast.error("Server error.  Please try again later.")
+                    break
+                default:
+                    toast.error(message || "An error occurred ")
+
+            } 
+        } else if (error.request) {
+            toast.error("Network error.  Please check your connection")
+        } else {
+            toast.error("An unexpected error occurred.")
         }
+
         return Promise.reject(error)
     }
 )
